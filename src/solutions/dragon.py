d9b5d6fcd181ff82e1e79e9571c99d0bef8e32cd
annika-hrt <annika.harter01@gmail.com>
2022-05-30 23:01:47 +0200

    Added update_explanation() to dragon

--- src/solutions/dragon.py
+++ src/solutions/dragon.py
@@ -80,5 +80,7 @@ class Dragon(SolvingTechniques):
         self.secondary_cells.extend(cells)
 
     def update_explanation(self):
-        self.explanation = f"""The candidate {self.candidate}"""
+        self.explanation = f"""The candidate {self.candidate} exists only twice in a row and a column and two of those cells share one box. 
+The cell {self.primary_cells[4]} builds the intersection of the other two cells.
+No matter where the candidate {self.candidate} is entered, because of the logical connection the {self.candidate} can be deleted in the cell {self.primary_cells[4]}."""
 
